0ebee7a17b8266511c329e0d9b9779410f349583
taoshen <2238334546@qq.com>
2020-07-01 21:15:57 +0800

    在clone下来的工程中添加新代码

--- src/Demo.java
+++ src/Demo.java
@@ -7,5 +7,7 @@ public class Demo {
         System.out.println("涛神年薪二十万");
         System.out.println("进行一次修改");
         System.out.println("进行第二次修改");
+        System.out.println("这里是clone拉取下来的工程");
+        //我们既然是从远程仓库中拉取下来的工程 所以自动就会变成一个Git本地仓库
     }
 }
